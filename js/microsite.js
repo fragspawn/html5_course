2c4689967ec5c6fd7a8a08a04f345e7e6c79e902
frag.spawn <frag.spawn@gmail.com>
2017-04-02 21:18:28 -0400

    first commit

---
+++ js/microsite.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+
+    // get all the links in the DOM and add an event to display the associated article.
+    var menuListItems = document.getElementsByTagName('a');
+    for(i=0;i<menuListItems.length;i++) {
+        if(menuListItems[i]) {
+            menuListItems[i].addEventListener('click', showArticle);
+        }
+    }
+
+    // enable the target page from URL, otherwise show the home page
+    var locale = window.location.href.substring(window.location.href.indexOf('#')+1);
+    if(locale.length > 0) {
+        document.getElementById(locale).style.display = 'block';
+    } else {
+        document.getElementById('html5').style.display = 'block';
+    }
+
+    // Handle display of sidemenu;
+    window.addEventListener('scroll', function() {
+        if(window.pageYOffset > 64) {
+            document.getElementById('sideMenu').style.left = '0';
+        } else {
+            sideMenu.style.left = '-42';
+        }
+    });
+}
+
+function showArticle() {
+    hideAllArticles();
+    document.getElementById(this.href.substring(this.href.indexOf('#')+1)).style.display = 'block';
+}
+
+function hideAllArticles() {
+    var contentItems = document.getElementsByTagName('article');
+    for(i=0;i<contentItems.length;i++) {
+        contentItems[i].style.display = 'none';
+    }
+}
